b603a3a673c686d537e75aeeb523372e4f4feab4
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2023-01-28 00:57:25 -0600

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,124 @@
+const basePath = process.cwd();
+const { MODE } = require(`${basePath}/constants/blend_mode.js`);
+const { NETWORK } = require(`${basePath}/constants/network.js`);
+
+const network = NETWORK.eth;
+
+// General metadata for Ethereum
+const namePrefix = "Rare Lazy Apepes";
+const description = "Rare Lazy Apepes is a community based project brought to you by a team of 2 amazing people (if we do say so ourselves).“R.L.A City ” is a reference to our leadership team who are here to help the R.L.A. community along the way as we continue to grow. Each team member brings knowledge and expertise creating a new and fun experience like no other! From artwork to event planning. Transparent communication to team building. Design to community growth. From dreams to reality…all this to say…this project has one thing in mind, growing with YOU!R.L.A City is here to help lead the community in the right way R.L.A way (Where community is government )Note: We are independent creators and are not affiliated with Yuga Labs , BAYC,LAZY LIONS, RAYC, MAYC,LAYC NOR Rarelabs.";
+const baseUri = "ipfs://NewUriToReplace";
+
+const solanaMetadata = {
+  symbol: "YC",
+  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
+  external_url: "https://www.youtube.com/c/hashlipsnft",
+  creators: [
+    {
+      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
+      share: 100,
+    },
+  ],
+};
+
+// If you have selected Solana then the collection starts from 0 automatically
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 10000,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Fur" },
+      { name: "Clothes" },
+      { name: "Mane" },
+      { name: "Hair" },
+      { name: "Mouth" },
+      { name: "Earring" },
+      { name: "HeadGear" },
+      { name: "Eyes" },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 3000,
+  height: 3000,
+  smoothing: true,
+};
+
+const gif = {
+  export: false,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
+};
+
+const background = {
+  generate: false,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 5,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  solanaMetadata,
+  gif,
+  preview_gif,
+};
